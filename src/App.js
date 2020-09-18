62a8f0f728f7acd93a4491333a6c0e4012e4b92f
Ana <angy.aa@gmail.com>
2020-09-18 21:03:32 +0200

    Add react router, material icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Header from "./common/header/Header";
 
 function App() {
-  return <div className="App">App works</div>;
+  return (
+    <Switch>
+      <Header />
+    </Switch>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import * as serviceWorker from "./serviceWorker";
 
@@ -9,7 +10,9 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
