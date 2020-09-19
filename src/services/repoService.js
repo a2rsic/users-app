762ab3384865290b65f45192592c495b0007caa0
Ana <angy.aa@gmail.com>
2020-09-19 15:37:51 +0200

    Create users and repos service

---
+++ src/services/repoService.js
@@ -0,0 +1,8 @@
+class RepoService {
+  fetchRepos(userId) {
+    const path = "";
+    // const response = await
+  }
+}
+
+export const repoService = new RepoService();
---
+++ src/services/userService.js
@@ -0,0 +1,8 @@
+class UserService {
+  fetchUsers() {
+    const path = "";
+    // const response = await
+  }
+}
+
+export const userService = new UserService();
