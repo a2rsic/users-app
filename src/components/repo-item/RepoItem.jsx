068f28e279f98ae52b19d96834c39be9af41c6e9
Ana <angy.aa@gmail.com>
2020-09-19 22:10:45 +0200

    Create repo item component

--- src/components/repo-item/RepoItem.jsx
+++ src/components/repo-item/RepoItem.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import "./RepoItem.scss";
+
 const RepoItem = () => {
-  return <div>repo item opened in new tab</div>;
+  return (
+    <div className="container">
+      <h2>Repos name</h2>
+      <ul>
+        <li>
+          <i>Licence type:</i>&nbsp; new
+        </li>
+        <li>
+          <i>Stargazers count:</i>&nbsp; 20
+        </li>
+        <li>
+          <i>Watchers count:</i>&nbsp; 20
+        </li>
+        <li>
+          <i>Forks count:</i>&nbsp; 20
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default RepoItem;
